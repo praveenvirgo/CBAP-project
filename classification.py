6677cf47cb48c2eb755327b3c4358a40c5f55a55
praveenvirgo <54227865+praveenvirgo@users.noreply.github.com>
2019-09-21 18:35:26 +0530

    Delete classification.py

--- classification.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd 
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-df = pd.read_csv('/home/praveen/learnPython/HH-master/Python/MachineLearning/Data/loan_data.csv')
-#df.info()
-print(df.describe())
